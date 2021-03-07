8fe50d5b335cd4d9d1921d04f8771a75019297c2
kean levreault <keanlevreault@keans-MacBook-Pro.local>
2021-03-07 13:39:57 -0800

    Kean's right livelihood

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
-const panels = document.querySelectorAll('.panel')
-const profile = document.querySelectorAll('.profileImg')
+// const panels = document.querySelectorAll('.panel')
+// const profile = document.querySelectorAll('.profileImg')
 
 
 
-panels.forEach(panel => {
-    panel.addEventListener('mouseover', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
+// panels.forEach(panel => {
+//     panel.addEventListener('mouseover', () => {
+//         removeActiveClasses()
+//         panel.classList.add('active')
        
-    })
-})
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
-}
+//     })
+// })
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
 
 // t1 = new TimelineMax();
 // t1.fromTo(profile, 1, {x:"0%"}, {x: "100%"});
