8e63e48b573dbc3a0bc10eebdd5beed7338cf3b7
kean levreault <keanlevreault@keans-MacBook-Pro.local>
2021-03-13 12:28:58 -0800

    kean's right livelihood

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 
-// const panels = document.querySelectorAll('.panel')
-// const profile = document.querySelectorAll('.profileImg')
 
+function toggleMenu(){
+    let hidden = document.getElementById('hiddenMenu');
+    if (hidden.style.display == 'none'){
+        hidden.style.display = 'block';
+    }else {hidden.style.display = 'none';
 
-
-// panels.forEach(panel => {
-//     panel.addEventListener('mouseover', () => {
-//         removeActiveClasses()
-//         panel.classList.add('active')
-       
-//     })
-// })
-// function removeActiveClasses() {
-//     panels.forEach(panel => {
-//         panel.classList.remove('active')
-//     })
-// }
-
-// t1 = new TimelineMax();
-// t1.fromTo(profile, 1, {x:"0%"}, {x: "100%"});
-
+    }
+}
