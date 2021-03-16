84243f2b7b547ed9fb4a14b84f4cdd35925ef57b
kean levreault <keanlevreault@keans-MacBook-Pro.local>
2021-03-15 19:24:40 -0700

    hostedBranch

--- public/script.js
+++ public/script.js
@@ -12,25 +12,8 @@ function toggleMenu(){
 
     }
 
+// const btnscroll = document.querySelector('#btnscroll');
 
-// const panels = document.querySelectorAll('.panel')
-// const profile = document.querySelectorAll('.profileImg')
-
-
-
-// panels.forEach(panel => {
-//     panel.addEventListener('mouseover', () => {
-//         removeActiveClasses()
-//         panel.classList.add('active')
-       
-//     })
-// })
-// function removeActiveClasses() {
-//     panels.forEach(panel => {
-//         panel.classList.remove('active')
-//     })
-// }
-
-// t1 = new TimelineMax();
-// t1.fromTo(profile, 1, {x:"0%"}, {x: "100%"});
-
+// btnscroll.addEventListener('click', function (){
+//     window.scrollTo(0, 0);
+// });
